8c11a171d97749d5f88ac03eac8568499d699938
Prince Kumar <princekumar201926@gmail.com>
2024-07-12 23:22:49 +0530

    update

--- routes/index.js
+++ routes/index.js
@@ -5,25 +5,25 @@ const upload = require("../config/multer");
 const { v4: uuidv4 } = require("uuid");
 const passport = require("passport");
 const userModel = require("../models/users");
+const productModel = require("../models/products");
+const sellerModel = require("../models/seller");
 
 const {registerUser, loginUser, logout} = require("../controller/authController");
-router.post("/upload", upload.single("file"), (req, res) => {
-  res.send("File uploaded!");
-});
-const {isLoggedIn} = require("../middleware/isLoggedIn");
 
-router.get("/uuid", (req, res) => {
-  res.send(`Generated UUID: ${uuidv4()}`);
-});
+const {isLoggedIn} = require("../middleware/isLoggedIn");
+const products = require("../models/products");
 
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index");
+router.get("/", async function (req, res, next) {
+  const products = await productModel.find();
+  res.render("index",{products});
 });
 
 router.get('/profile', isLoggedIn, async (req, res) => {
-  // Render the profile page or handle the profile logic
-  res.render('profile'); // Ensure you have a 'profile.ejs' or appropriate view template
+  const user = await userModel.findOne({email : req.user.email})
+  .populate("products");
+  
+  res.render('profile', {user}); 
 });
 
 router.get("/signup", function (req, res, next) {
@@ -31,19 +31,97 @@ router.get("/signup", function (req, res, next) {
 });
 router.post("/signup", registerUser);
 
-
-
 router.get("/login", function (req, res, next) {
   res.render("login");
 });
 
 router.post("/login", loginUser);
 
-
-
-
-
 router.get("/logout", logout);
+
+router.post("/upl-profile-picture", isLoggedIn,upload.single("upl-img"),async function(req,res, next){
+  const user = await userModel.findOne({email: req.user.email});
+  user.profile_picture = req.file.buffer;
+  await user.save();
+    res.redirect("/profile");
+
+})
+
+router.get("/add-product",isLoggedIn, function(req, res, next){
+  res.render("addProduct");
+});
+
+router.post("/add-product", isLoggedIn, upload.single("image"), async function(req, res, next){
+  try {
+    const user = await userModel.findOne({ email: req.user.email });
+
+    if (user && user.seller) {
+      let product = await productModel.create({
+        user: user._id,
+        image: req.file.buffer,
+        name: req.body.name,
+        price: req.body.price,
+        discount: req.body.discount,
+        description: req.body.description,
+        quantity: req.body.quantity,
+      });
+
+      user.products.push(product._id);
+      await user.save();
+
+      res.redirect("/profile");
+    } else {
+      res.send("Not a seller");
+    }
+  } catch (err) {
+    res.send(err.message);
+  }
+  
+  
+});
+
+router.get("/become-seller",isLoggedIn, function(req, res, next){
+  res.render("seller.ejs")
+});
+
+router.post("/become-seller",isLoggedIn, async function(req, res, next){
+    try {
+    const user = await userModel.findOne({ email: req.user.email });
+
+      console.log(user);
+        
+        // Create new seller record
+        let newSeller = await sellerModel.create({
+            name:req.body.name,
+            bankAccount:req.body.bankAccount,
+            gstNumber:req.body.gstNumber,
+            aadharNumber: req.body.aadharNumber,
+            panCard: req.body.panCard,
+        });
+        console.log("first");
+        await newSeller.save();
+
+        user.seller = true;
+        await user.save();
+
+        res.redirect("/profile");
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Error submitting seller application');
+    }
+});
+
+router.get("/add-to-cart/:id", isLoggedIn, async function(req, res,next){
+  const user = await userModel.findOne({ email: req.user.email });
+  
+
+});
+
+
+  
+
+
+
 
 
 module.exports = router;
