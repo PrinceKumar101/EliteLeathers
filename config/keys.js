f181113858143fc5b7a2917823e1d9b88522c9f2
Prince Kumar <princekumar201926@gmail.com>
2024-07-31 23:28:54 +0530

    update change in keys

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,5 @@
 JWT_KEYS = process.env.JWT_KEYS;  
 EXPRESS_SECRET_KEY = process.env.EXPRESS_SECRET_KEY;
 URL = process.env.URL;
+EMAIL_USER = process.env.EMAIL_USER
+EMAIL_PASS = process.env.EMAIL_PASS
